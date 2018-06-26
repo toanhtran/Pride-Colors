4ee0cdeda85f64b86f13817d17831563840bce7c
toanhtran <toanh.t.tran@gmail.com>
2018-06-26 15:25:23 -0600

    first commit

---
+++ Pride Colors/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  Pride Colors
+//
+//  Created by ToAnh T. Tran on 6/26/18.
+//  Copyright © 2018 ToAnh T. Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	@IBAction func turnRedOn(_ sender: Any) {
+		view.backgroundColor = .red
+	}
+	
+	@IBAction func turnOrangeOn(_ sender: Any) {
+		view.backgroundColor = .orange
+	}
+	
+	@IBAction func turnYellowOn(_ sender: Any) {
+		view.backgroundColor = .yellow
+	}
+	
+	
+	
+	@IBAction func turnLaSalleGreenOn(_ sender: Any) {
+		view.backgroundColor = .green
+	}
+	
+	
+	@IBAction func turnBlueOn(_ sender: Any) {
+		view.backgroundColor = .blue
+	}
+	
+	
+	@IBAction func turnPurpleOn(_ sender: Any) {
+		view.backgroundColor = .purple
+	}
+	
+	@IBAction func turnWhiteOn(_ sender: Any) {
+		view.backgroundColor = .white
+	}
+	
+	
+}
+
